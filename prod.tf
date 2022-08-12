8ff09cca586f77f69a283d872369058d71c4b949
wilma <wilminhasilva@gmail.com>
2022-08-11 22:03:32 -0300

    Web instances and elastic ip

--- prod.tf
+++ prod.tf
@@ -42,6 +42,26 @@ resource "aws_security_group" "prod_web" {
   tags = {
     "Terraform" : "true"
   }
+}
 
+resource "aws_instance" "prod_web" {
+  ami = "ami-05d5fc1132b8df2e2"
+  instance_type = "t2.nano"
 
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true"
+  }
+  
 }
