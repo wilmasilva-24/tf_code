5811c81edbd224ae91e5e74089d9cda29f849b51
wilma <wilminhasilva@gmail.com>
2022-08-11 21:19:07 -0300

    Defined initial web secutity group

--- prod.tf
+++ prod.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
-  bucket = "tf-course-20220810"
+  bucket = "tf-course-202208101"
 }
 
 resource "aws_s3_bucket_acl" "tf_course_acl" {
@@ -12,6 +12,36 @@ resource "aws_s3_bucket_acl" "tf_course_acl" {
   acl    = "private"
 }
 
-resource "aws_default_vpc" "default" {
-  
+resource "aws_default_vpc" "default" { }
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+
+
 }
